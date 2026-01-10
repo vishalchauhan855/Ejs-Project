b436d7fcfaf00324c8d4c7df73a25dcf0c0bb118
vishalchauhan <vishalchauhan9265211@gmail.com>
2026-01-10 12:15:23 +0530

    Merge remote changes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express=require('express')
-const homeContent = require('./views/src/layout/conten')
+// const homeContent = require('./views/src/layout/conten')
+// const aboutContent=require("./views/src/layout/conten")
+const { homeContent, aboutContent } = require("./views/src/layout/conten");
+
 const app=express()
 app.set("view engine","ejs")
 require("dotenv").config()
@@ -7,17 +10,18 @@ const PORT=process.env.PORT||5000
 
 app.use(express.static("Public"))
 
-app.get('/',(req,res)=>{
-    res.render("src/pages/Home",{
-        title:"home",
-        allData:homeContent
-    })
-})
-app.listen(PORT,()=>console.log(`server is connected http://localhost${PORT}`))
+app.get("/", (req, res) => {
+  res.render("src/pages/Home", {
+    title: "home",
+    allData: homeContent
+  });
+});
+
 
 app.get("/about",(req,res)=>{
     res.render("src/pages/about",{
-        title:"about"
+        title:"about",
+        allData:aboutContent
     })
 })
 
@@ -33,3 +37,4 @@ app.get("/readmore",(req,res)=>{
         title:"readmore"
     })
 })
+app.listen(PORT,()=>console.log(`server is connected http://localhost${PORT}`))
--- views/src/layout/conten.js
+++ views/src/layout/conten.js
@@ -1,2 +1,30 @@
-const homeContent=[{id:132,name:"vishal",screenShot:"images/ejs_image.jpeg"},{id:324,name:"vikas"},{id:678,name:"raj"}]
-module.exports=homeContent
+const homeContent = [
+  { id: 132, name: "vishal", screenShot: "images/ejs_image.jpeg" },
+  { id: 324, name: "vikas" },
+  { id: 678, name: "raj" }
+];
+
+const aboutContent = [
+  {
+    id: 1,
+    title: "Who We Are",
+    description: "We are a team focused on building scalable web applications.",
+    screenShot: "images/about_1.jpeg"
+  },
+  {
+    id: 2,
+    title: "Our Mission",
+    description: "Our mission is to deliver clean, fast, and secure solutions.",
+    screenShot: "images/about_2.jpeg"
+  },
+  {
+    id: 3,
+    title: "Why Choose Us",
+    description: "We focus on performance, maintainability, and user experience."
+  }
+];
+
+module.exports = {
+  homeContent,
+  aboutContent
+};
